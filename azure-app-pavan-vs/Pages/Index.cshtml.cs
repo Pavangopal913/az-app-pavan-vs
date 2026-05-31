9bc706a6db0589fed4399f2e2d3105e233ba839a
pavangopal <pavangopal913@gmail.com>
2026-05-31 16:54:59 +0530

    Added Configuration Greeting message

--- azure-app-pavan-vs/Pages/Index.cshtml.cs
+++ azure-app-pavan-vs/Pages/Index.cshtml.cs
@@ -5,9 +5,16 @@ namespace azure_app_pavan_vs.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _configuration["Greeting"];
         }
     }
 }
